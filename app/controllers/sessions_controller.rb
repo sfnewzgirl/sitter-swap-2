d94c12cb53ca556c87865b26051f7cdb4cb43ef7
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-30 20:31:57 -0700

    mocks up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,28 @@
 class SessionsController < ApplicationController
 
   def new
-      @person = Person.new
+    @person = Person.new
   end
 
   def create
+    @person = Person.confirm(person_params)
+    if @person
+      login(@person)
+      redirect_to 'people' + @person.id
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
+    logout(@person)
+    redirect_to root_path
   end
 
   private
 
   def person_params
+    params.require(:person).permit(:first_name, :last_name, :phone_number, :city, :email, :password)
   end
 
 end
