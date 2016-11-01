64080bcb03d0ff2ad9c255b66ac7ae776056e366
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-01 10:25:19 -0700

    streamlines some bootstrap grids

--- app/controllers/family_people_controller.rb
+++ app/controllers/family_people_controller.rb
@@ -79,8 +79,5 @@ class FamilyPeopleController < ApplicationController
       params.permit(:role, :family_id)
     end
 
-    def create_sitter_params
-      sitter_info = params.require(:person).permit(:first_name, :last_name, :phone_number, :city, :email, :password)
-      create_sitter_params = sitter_info.merge({:family_id, :person_id, :role})
-    end
+    
 end
