8a7128e3646366fd8451ff31b5936a7b958c6d8c
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-11-03 09:04:48 -0700

    moves current person to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
   protect_from_forgery with: :exception
+
+  def login(person)
+    session[:id] = person.id
+    @current_person = person
+    puts 'SESSION-HELPER-ID', person.id
+  end
+
+  def current_person
+    puts 'SESSION-HELPER-CURRENT-PERSON'
+    @current_person ||= Person.find_by_id(session[:id])
+  end
+
+  def logout
+    @current_person = session[:id] = nil
+  end
+
 end
